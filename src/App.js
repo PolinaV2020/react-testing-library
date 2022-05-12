fdf4bb3e3a555da4d2634d0f4fb50f8a97b22e92
polinav2020 <polinapiksi2019@mail.ru>
2022-05-12 13:06:57 +0300

    add: input event testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [toggle, seToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   const [inputValue, seInputValue] = useState(false);
 
-  const onClick = () => seToggle(prev => !prev)
+  const onClick = () => setToggle((prev) => !prev);
 
   useEffect(() => {
-    setTimeout(() => setData({}), 100)
-  }, [])
+    setTimeout(() => setData({}), 100);
+  }, []);
 
   return (
     <div>
-      {toggle === true && <div data-testid="toggle-div">toggle</div>}
+      {toggle && <div data-testid="toggle-div">toggle</div>}
       <h1>hello world!</h1>
-      <h2 data-testid="toggle-button">{inputValue}</h2>
-      {data && <div style={{backgroundColor: "coral"}}>data</div>}
-      <button data-testid="input-value" onClick={onClick}>Click me</button>
-      <input onChange={(e) => seInputValue(e.target.value)} type="text" placeholder="Enter your text"></input>
+      <h2 data-testid="input-value">{inputValue}</h2>
+      {data && <div style={{ backgroundColor: "coral" }}>data</div>}
+      <button data-testid="toggle-button" onClick={onClick}>
+        Click me
+      </button>
+      <input
+        onChange={(e) => seInputValue(e.target.value)}
+        type="text"
+        placeholder="Enter your text"
+      ></input>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
 describe("App component", () => {
-
-  test('Renders App elements', () => {
+  test("Renders App elements", () => {
     render(<App />);
     const title = screen.getByText(/hello world/i);
-    const button = screen.getByRole('button')
-    const input = screen.getByPlaceholderText(/Enter your text/i)
+    const button = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/Enter your text/i);
     expect(title).toBeInTheDocument();
     expect(button).toBeInTheDocument();
     expect(input).toBeInTheDocument();
@@ -18,27 +18,33 @@ describe("App component", () => {
     render(<App />);
     const text = screen.queryByText(/hidden text/i);
     expect(text).toBeNull();
-  })
+  });
 
   test("Renders data text", async () => {
     render(<App />);
     const dataText = await screen.findByText(/data/i);
     expect(dataText).toBeInTheDocument();
-    expect(dataText).toHaveStyle({backgroundColor: "coral"});
-  })
+    expect(dataText).toHaveStyle({ backgroundColor: "coral" });
+  });
 
+  test("Toggle event", () => {
+    render(<App />);
+    const toggleButton = screen.getByTestId("toggle-button");
+    expect(screen.queryByTestId("toggle-div")).toBeNull();
+    fireEvent.click(toggleButton);
+    expect(screen.getByTestId("toggle-div")).toBeInTheDocument();
+    fireEvent.click(toggleButton);
+    expect(screen.queryByTestId("toggle-div")).toBeNull();
+  });
 
-  // test('Toggling', () => {
-  //   render(<App />);
-  //   const toggleButton = screen.getByTestId('toggle-button');
-  //   expect(toggleButton).toBeInTheDocument();
-  //   expect(screen.queryByTestId('toggle-div')).toBeNull();
-  //   fireEvent.click(toggleButton);
-  //   expect(screen.getByTestId('toggle-div')).toBeInTheDocument();
-  // });
-})
-
-
-
-
-
+  test("Input event", () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/Enter your text/i);
+    expect(screen.queryByTestId("input-value")).toContainHTML("");
+    fireEvent.input(input, {
+      target: { value: "testtesttest" }
+    });
+    userEvent.type(input, "test"); // The same as fireEvent, but closer to user behavior
+    expect(screen.queryByTestId("input-value")).toContainHTML("test");
+  });
+});
